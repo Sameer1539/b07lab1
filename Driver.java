9eef9bc423b446c01be942ec2adfcff3378f8b4b
Sameer1539 <118702965+Sameer1539@users.noreply.github.com>
2024-09-29 22:04:32 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,53 @@
+import java.io.IOException;
 public class Driver {
     public static void main(String [] args) {
+        
         Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
+        double [] c1 = {6, 5, -2};
+        int [] ex1 = {1, 3, 2};
+        Polynomial p1 = new Polynomial(c1, ex1);
+        double [] c2 = {3, -4, 2};
+        int [] ex2 = {2, 1, 0};
+        Polynomial p2 = new Polynomial(c2, ex2);
         Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
-        if(s.hasRoot(1))
-            System.out.println("1 is a root of s");
-        else
-            System.out.println("1 is not a root of s");
-    }
+
+        // The output should be 2.0x^(1) + 5.0x^(3) + 1.0x^(2) + 2.0x^(0) +
+        // or 2x + 5x^3 + x^2 + 2  
+        for (int i = 0; i < s.coefficients.length; i++){
+            System.out.print(s.coefficients[i] + "x^(" + s.exponents[i] + ") + ");
+        }
+        
+        // Test the multiply method
+        double[] c3 = {6.0, -2.0, 4.0, -2.0};
+        int[] ex3 = {0, 1, 3, 5};
+        Polynomial p3 = new Polynomial(c3, ex3);
+        
+        double[] c4 = {4, -4, 3};
+        int[] ex4 = {2, 3, 1};
+        Polynomial p4 = new Polynomial(c4, ex4);
+
+        // The output should be 18.0x^(2) + -32.0x^(3) + 18.0x^(1) + 20.0x^(4) + 16.0x^(5) + -22.0x^(6) + -8.0x^(7) + 8.0x^(8) +
+        Polynomial product = p3.multiply(p4);
+        System.out.println();
+        for (int i = 0; i < product.coefficients.length; i++){
+            System.out.print(product.coefficients[i] + "x^(" + product.exponents[i] + ") + ");
+        }
+        System.out.println();
+        // Check for redudant exponents
+        for (int i = 0; i < product.exponents.length; i++){
+            System.out.print(product.exponents[i]);
+        }
+
+        // Example polynomial: 5 - 3x^2 + 7x^8
+        double[] coeffs = {5, -3, 7};
+        int[] expos = {0, 2, 8};
+        p = new Polynomial(coeffs, expos);
+
+        // Save the polynomial to a file
+        try{
+            p.saveToFile("polynomial.txt");
+        } catch (IOException e) {   
+        }
+    }   
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,47 +1,218 @@
-public class Polynomial {
-    //i.
-    double[] coefficients;
+// NTS: The exponents array is based on the coefficients so they 
+// should be the same length.
 
-    //ii.
-    public Polynomial() {
-        coefficients = new double[]{0};
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Polynomial {
+    // Two fields represent the non-zero coefficients and the corresponding 
+    // exponents
+    double[] coefficients;
+    int[] exponents;
+
+    // The polynomial is set to zero when the arrays are empty
+    public Polynomial()  {
+        coefficients = new double[0];
+        exponents = new int[0];
     }
 
-    //iii.
-    public Polynomial(double[] args) {
-        // Don't set coefficients to args to prevent aliasing
-        coefficients = new double[args.length];
-        for (int i = 0; i < args.length; i++) {
-            coefficients[i] = args[i];  
+    // Sets the arrays accordingly to given arguments
+    public Polynomial(double[] coeffs, int[] expos) {
+        coefficients = new double[coeffs.length];
+        exponents = new int[expos.length];
+        
+        for (int i = 0; i < coeffs.length; i++) {
+            coefficients[i] = coeffs[i];
+            exponents[i] = expos[i];  
         }
     } 
 
-    //iv.
-    public Polynomial add(Polynomial other) {
-        int maxLength = Math.max(coefficients.length, other.coefficients.length);
-        double[] result = new double[maxLength];
+    public Polynomial(File file){
+        Scanner input = new Scanner(file);
+        String polynomial = input.nextLine();
+        input.close();
 
-        for (int i = 0; i < maxLength; i++) {
-            if (i < coefficients.length) {
-                result[i] += coefficients[i];
+        polynomial = polynomial.replace(" ", "");
+        ArrayList<Double> coeffsList = new ArrayList<>();
+        ArrayList<Integer> exposList = new ArrayList<>();
+
+        String[] terms = polynomial.split("(?=[+-])");
+
+        for (String term: terms){
+            double coeff;
+            int expo;
+            
+            if (term.contains("x")){
+                String[] parts = term.split("x\\^?");
+                
+                if (parts[0].equals("") || parts[0].equals("+")) {
+                    coeff = 1.0;
+                } else if (parts[0].equals("-")) {
+                    coeff = -1.0;
+                } else{
+                    coeff = Double.parseDouble(parts[0]);
+                }
+
+                if (parts.length > 1 && !parts[1].isEmpty()) {
+                    expo = Integer.parseInt(parts[1]);
+                } else {
+                    expo = 1;  
+                }
             }
-            if (i < other.coefficients.length) {
-                result[i] += other.coefficients[i];
+            else {
+                // If there's no "x", it's a constant term with exponent 0
+                coeff = Double.parseDouble(term);
+                expo = 0;
             }
+            coeffsList.add(coeff);
+            exposList.add(expo);
         }
-        return new Polynomial(result);
+        coefficients = new double[coeffsList.size()];
+        exponents = new int[exposList.size()];
+        for (int i = 0; i < coeffsList.size(); i++) {
+            coefficients[i] = coeffsList.get(i);
+            exponents[i] = exposList.get(i);
+        }
     }
 
-    //v.
+    public void saveToFile(String fileName) throws IOException {
+        FileWriter output = new FileWriter(fileName);
+        
+        String polynomialStr = "";
+        
+        for (int i = 0; i < coefficients.length; i++) {
+            double coeff = coefficients[i];
+            int expo = exponents[i];
+
+            // Only add non-zero coefficients
+            if (coeff != 0){
+
+                // Add "+" for positive coefficients, except for the first term
+                if (i >= 1 && coeff > 0){
+                    polynomialStr += "+";
+                }
+                if (expo == 0) {
+                    // Constant term
+                    polynomialStr += coeff;
+                } else if (coeff == 1) {
+                    // Coefficient is 1, so just add the x
+                    polynomialStr += "x";
+                } else if (coeff == -1) {
+                    // Coefficient is -1, so just add the x with a minus sign
+                    polynomialStr += "-x";
+                }
+                else {
+                    // Non-constant terms
+                    polynomialStr += coeff + "x";
+                }
+                // Add the exponent when it's not 1
+                if (expo > 1) {
+                    polynomialStr += expo;
+                }
+            }
+        }
+        output.write(polynomialStr);
+        output.close();
+    }
+
+    /// Example:
+    /// 6 - 2x + 4x^3 - 2x^5 [6, -2, 4, -2] :: [0, 1, 3, 5]
+    /// 3x + 4x^2 - 4x^3 [3, 4, -4] :: [1, 2, 3]
+    /// 6 + x + 4x^2 - 2x^5 [6, 1, 4, -2] :: [0, 1, 2, 5]
+    public Polynomial add(Polynomial other) {
+        ArrayList<Double> sum_coeffs = new ArrayList<>();
+        ArrayList<Integer> sum_expos = new ArrayList<>();
+        
+        // Add from the current polynomial's fields
+        for (int i = 0; i < exponents.length; i++) {
+            sum_coeffs.add(coefficients[i]);
+            sum_expos.add(exponents[i]);
+        }
+        
+        for (int i = 0; i < other.exponents.length; i++) {
+            int index = sum_expos.indexOf(other.exponents[i]);
+
+            if (index != -1) {
+                // If the exponents are the same, add the coefficients
+                double tempCoeff = sum_coeffs.get(index) + other.coefficients[i];
+                if (tempCoeff != 0) {
+                    sum_coeffs.set(index, tempCoeff);
+                } else {
+                    sum_coeffs.remove(index);
+                    sum_expos.remove(index);
+                }
+                } else {
+                // If the exponent doesn't exist, add it as a new term
+                if (other.coefficients[i] != 0) {
+                    sum_coeffs.add(other.coefficients[i]);
+                    sum_expos.add(other.exponents[i]);
+                }
+            }
+        }
+            // Convert arraylists back to arrays:
+            double[] result_coeffs = new double[sum_coeffs.size()];
+            int[] result_expos = new int[sum_expos.size()];
+
+            for (int i = 0; i < sum_coeffs.size(); i++){
+                result_coeffs[i] = sum_coeffs.get(i);
+                result_expos[i] = sum_expos.get(i);
+            }
+        return new Polynomial(result_coeffs, result_expos);
+    }
+    
+    public Polynomial multiply(Polynomial other){
+        ArrayList<Double> product_coeffs = new ArrayList<>();
+        ArrayList<Integer> product_expos = new ArrayList<>();
+        
+        // Multiply each term of one polynomial by each term of the other polynomial
+        for (int i = 0; i < coefficients.length; i++) {
+            for (int j = 0; j < other.coefficients.length; j++) {
+                double tempCoeff = coefficients[i] * other.coefficients[j];
+                int tempExpo = exponents[i] + other.exponents[j];
+
+                int index = product_expos.indexOf(tempExpo);
+                if (index != -1) {
+                    double newCoeff = product_coeffs.get(index) + tempCoeff;
+                    // If the exponent already exists, add the coefficients
+                    if (newCoeff != 0) {
+                        product_coeffs.set(index, newCoeff);
+                    }
+                    else {
+                        product_coeffs.remove(index);
+                        product_expos.remove(index);
+                    }
+                } else {
+                    // If the exponent doesn't exist, add the new term
+                    if (tempCoeff != 0) {
+                        product_coeffs.add(tempCoeff);
+                        product_expos.add(tempExpo);
+                    }
+                }
+            }
+        }
+        // Convert arraylists back to arrays:
+        double[] result_coeffs = new double[product_coeffs.size()];
+        int[] result_expos = new int[product_expos.size()];
+
+        for (int i = 0; i < product_coeffs.size(); i++){
+            result_coeffs[i] = product_coeffs.get(i);
+            result_expos[i] = product_expos.get(i);
+        }
+
+        return new Polynomial(result_coeffs, result_expos);
+    }
+
     public double evaluate(double x){
         double result = 0;
         for (int i = 0; i < coefficients.length; i++) {
-            result += coefficients[i] * Math.pow(x, i);
+            result += coefficients[i] * Math.pow(x, exponents[i]);
         }
         return result;
     }
 
-    //vi.
     public boolean hasRoot(double arg) {
         return evaluate(arg) == 0;
     }
